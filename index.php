d4b5eb82d2e57fd864936cfb1ab0438845c0ddcb
dragon-king <113839077+DragonKing-BIN@users.noreply.github.com>
2024-05-08 02:35:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 <head>
     <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8; X-Wap-Proxy-Cookie=none" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <title>Zhea丨源码演示</title>
-    <link type="text/css" href="app/style/chat.css?v=1.2" rel="stylesheet" />
+    <title>龙王网站</title>
+    <link type="text/css" href="chat.css?v=1.2" rel="stylesheet" />
 </head>
 <body>
-<div class="top"><span class="name">柚儿园丨多人在线聊天</span></div>
+<div class="top"><span class="name">龙王聊天室</span></div>
 <?php
-require_once 'app/app.php';
+require_once 'app.php';
 if(empty(@$_COOKIE[KEYS.'_name'])){
 	echo '<div class="write login">                
                 <input type="text" value="'.rand_nick().'" id="nick" maxlength="5" placeholder="请随便输入昵称.." />                
-                <a href="javascript:;" id="login" class="write-link send">进♂入</a>
+                <a href="javascript:;" id="login" class="write-link send">登录</a>
 </div>';	      
 } 
 echo '<div class="talk mk-chat-box">     
@@ -22,7 +22,7 @@ echo '<div class="talk mk-chat-box">
                 <a href="javascript:;" id="send" class="write-link send" onclick="send()">发送</a>
 </div>'; 
 ?>
-<script src="app/style/jquery.min.js"></script>
-<script src="app/style/chat.min.js"></script>
+<script src="jquery.min.js"></script>
+<script src="chat.min.js"></script>
 </body>
 </html>
