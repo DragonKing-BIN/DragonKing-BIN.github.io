0fb2702cf0c9279c0a6ad37ff1b95e08ac0bb01f
dragon-king <113839077+DragonKing-BIN@users.noreply.github.com>
2024-05-12 14:38:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def handler(websocket, path):
         connected.remove(websocket)
 
 # 启动 WebSocket 服务器
-start_server = websockets.serve(handler, "localhost", 8765)
+start_server = websockets.serve(handler, "0.0.0.0", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
