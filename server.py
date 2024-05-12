02340dd71ced318b9adb0a7c56f947452e09449f
dragon-king <113839077+DragonKing-BIN@users.noreply.github.com>
2024-05-12 14:43:41 +0800

    Update and rename main.py to server.py

---
+++ server.py
@@ -0,0 +1,18 @@
+import asyncio
+import websockets
+
+connected = set()
+
+async def handler(websocket, path):
+    connected.add(websocket)
+    try:
+        async for message in websocket:
+            await asyncio.wait([ws.send(message) for ws in connected])
+    finally:
+        connected.remove(websocket)
+
+start_server = websockets.serve(handler, "0.0.0.0", 8765)
+
+print("WebSocket chatroom server started. Listening on port 8765.")
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
